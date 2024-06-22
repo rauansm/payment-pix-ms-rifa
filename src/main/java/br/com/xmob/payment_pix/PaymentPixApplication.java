9f31226a1764e19ec1167690853855428b7fca78
Rauan Santos <rauanmoreira259@gmail.com>
2024-06-22 19:20:33 -0300

    init project

---
+++ src/main/java/br/com/xmob/payment_pix/PaymentPixApplication.java
@@ -0,0 +1,13 @@
+package br.com.xmob.payment_pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/xmob/payment_pix/PaymentPixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xmob.payment_pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
